2eb785e18ec3767640f022bfebb42fd9adbc8876
swaroop0202 <swaroopk372@gmail.com>
2024-04-23 18:56:20 +0530

    message

--- mysql.sh
+++ mysql.sh
@@ -4,3 +4,37 @@ USERID=$(id -u)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+echo "please enter db password"
+read mention
+
+G="\e[31m]"
+R="\e[32m]"
+Y="\e[33m]"
+N="\e[0m]"
+
+if [ $USERID -ne 0 ]
+then
+echo "please run this script as root user"
+else
+echo "you are  a super user"
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+    echo -e "$2...$R failure $N "
+    else
+    echo -e "$2...$G success $N"
+    fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql"
+
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass $mention
+
+
+
